77b0b539e52b802a093bcda949701a1535deef6b
akifberabasar <akifbera583@gmail.com>
2023-12-30 18:04:33 +0300

    Maven kodları atıldı.

---
+++ MavenProjeOrnek1/src/main/java/tr/edu/medipol/yazilimort/Main.java
@@ -0,0 +1,20 @@
+package tr.edu.medipol.yazilimort;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String hataliMetin = " m  e  dip o   l";
+		
+		// hatali metni duzelt
+		
+		String duzgunMetin = StringUtils.deleteWhitespace(hataliMetin);
+		
+		System.out.println(hataliMetin);
+		System.out.println(duzgunMetin);
+
+	}
+
+}
