578b091b43e646659ed93a4d396c04d146adf3b2
Your Name <you@example.com>
2023-12-31 15:22:24 +0300

    Birim testi yazıldı.

---
+++ BirimTestOrnek1/src/test/java/tr/edu/medipol/yazilimort/HesaplamalarTest.java
@@ -0,0 +1,41 @@
+package tr.edu.medipol.yazilimort;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HesaplamalarTest {
+
+	/*
+	@Test
+	public void testFaktoriyelRec() {
+	  int sonuc = Hesaplamalar.faktoriyelRec(5);
+	  assertEquals(120,sonuc);
+	}
+
+	@Test
+	public void testFaktoriyelLoop() {
+		int sonuc = Hesaplamalar.faktoriyelLoop(5);
+		assertEquals(120,sonuc);
+		
+	}
+	*/
+	@Test
+	public void testToplama() {
+		int sonuc = Hesaplamalar.topla(2, 3);
+		assertEquals(5,sonuc);	
+	}
+	
+	@Test
+	public void testCikarma() {
+		int sonuc1 = Hesaplamalar.cikart(5, 1);
+		assertEquals(4,sonuc1);	
+	}
+	
+	@Test
+	public void testCarpma() {
+		int sonuc2 = Hesaplamalar.carpma(2, 3);
+		assertEquals(6,sonuc2);	
+	}
+
+}
